b2efcc15f91c520a6a49a96a1f5b74b688d8e902
Daria Kharkovskaya <dicsi59@gmail.com>
2024-10-29 20:04:21 +0300

    Код на ревью V1.0

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -3,22 +3,72 @@ package manager;
 import task.Task;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> history = new ArrayList<>();
-    private static final int HISTORY_SIZE = 10;
+    private Map <Integer, Node<Task>> historyTasks = new LinkedHashMap<>();
+    private Node<Task> tail;
 
     @Override
     public void add(Task task) {
-        if (history.size() >= HISTORY_SIZE) {
-            history.removeFirst();
+        if (historyTasks.containsKey((int)task.getId())){
+            remove((int)task.getId());
         }
-        history.add(task);
+        linkLast(task);
     }
-
+    @Override
+    public void remove(int id) {
+        removeNode(historyTasks.get(id));
+    }
     @Override
     public List<Task> getHistory() {
-        return history;
+        return getTasks();
+    }
+
+    public void linkLast(Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> newNode = new Node<>(null, task, oldTail);
+        tail = newNode;
+        historyTasks.put((int)task.getId(), tail);
+        if (oldTail == null)
+            tail = newNode;
+        else
+            oldTail.prev= newNode;
+    }
+    public List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        for (Map.Entry<Integer, Node<Task>> listTask : historyTasks.entrySet()){
+            tasks.add(listTask.getValue().data);
+        }
+        return  tasks;
+    }
+
+    private void removeNode(Node task) {
+        if (task.next == null){
+            task.prev.next = null;
+        } else if (task.prev == null) {
+            task.next.prev = null;
+        } else {
+            Node next = task.next;
+            Node prev = task.prev;
+            prev.next = next;
+            next.prev = prev;
+        }
+        task.prev = null;
+        task.next = null;
+    }
+
+    class Node<E> {
+        public E data;
+        public Node<E> next;
+        public Node<E> prev;
+
+        public Node(Node<E> prev, E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
     }
 }
